0a19fb0fabb71a8fe7aba6b598133a2ff9ed4014
Chris <cpaterson152@gmail.com>
2023-06-01 15:30:49 -0700

    Dynamically search depending on query

--- index.js
+++ index.js
@@ -2,8 +2,39 @@
 let searchForm = document.querySelector('#search-form');
 searchForm.addEventListener('submit', function(e) {
     e.preventDefault();
-})
 
+
+    //AJAX REQUEST
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+    
+            let response = JSON.parse(xhttp.responseText);
+            let videoData = response.items.map(function(item) {
+                return item.snippet;
+            })
+    
+            let container = document.querySelector('#response');
+            container.innerHTML = '';
+            videoData.forEach(function(video) {
+                let videoDiv = document.createElement('div');
+                videoDiv.classList.add('video-div');
+                videoDiv.innerHTML = `
+                    <img src='${video.thumbnails.high.url}'>
+                    <h4>${video.title}</h4>
+                    <p>${video.channelTitle}</p>
+                    <p>${new Date(video.publishTime).toLocaleDateString()}</p>
+    
+                `
+                container.appendChild(videoDiv);
+            })
+        }
+    };
+    let textValue = document.querySelector('#search-bar').value;
+    xhttp.open("GET", `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${textValue}&maxResults=12&key=AIzaSyAokMyi5gwYyITDzEGCWY0dmSZj-DW3Lhs`, true);
+    xhttp.send();
+})
 
 let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
@@ -29,5 +60,6 @@ xhttp.onreadystatechange = function() {
         })
     }
 };
-xhttp.open("GET", "https://www.googleapis.com/youtube/v3/search?part=snippet&q=commitmentrelationships&maxResults=12&key=AIzaSyAokMyi5gwYyITDzEGCWY0dmSZj-DW3Lhs", true);
+
+xhttp.open("GET", `https://www.googleapis.com/youtube/v3/search?part=snippet&q=commitmentrelationships&maxResults=12&key=AIzaSyAokMyi5gwYyITDzEGCWY0dmSZj-DW3Lhs`, true);
 xhttp.send();
